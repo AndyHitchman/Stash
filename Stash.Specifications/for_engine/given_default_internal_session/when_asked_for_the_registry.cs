55b38d33db27424125688ef06522e9ed65472af5
Andy Hitchman <andy@hitchman.net>
2010-01-12 07:45:48 +1100

    Rename Registration to Registry

---
+++ Stash.Specifications/for_engine/given_default_internal_session/when_asked_for_the_registry.cs
@@ -0,0 +1,20 @@
+namespace Stash.Specifications.for_engine.given_default_internal_session
+{
+    using Configuration;
+    using Engine;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class when_asked_for_the_registry
+    {
+        [Test]
+        public void it_should_return_the_provided_registration()
+        {
+            var fakeRegistry = new Registry();
+
+            var sut = new DefaultInternalSession(fakeRegistry);
+
+            sut.Registry.ShouldEqual(fakeRegistry);
+        }
+    }
+}
