4e12c9c8765a1102956ced5247c8db3121e28000
Andy Hitchman <andy@hitchman.net>
2010-01-12 07:30:17 +1100

    Outline session

--- Stash/Engine/DefaultInternalSession.cs
+++ Stash/Engine/DefaultInternalSession.cs
@@ -40,10 +40,11 @@ namespace Stash.Engine
 
         public virtual void Enroll<TGraph>(PersistenceEvent<TGraph> persistenceEvent)
         {
-            //Calculate indexes, maps and reduces on track. This should allow any changes to be determined by comparison,
+            //Calculate indexes, maps and reduces on tracked graphs. This should allow any changes to be determined by comparison,
             //saving unecessary work in the backing store.
             //Keep a local cache of indexes, maps and reduces for graphs tracked in the session. Go here before hitting the
             //backing store. Reduces may be out of date once retrieved.
+            //Exclude destroyed graphs from results.
             //Reduces must be calculated by a background process to ensure consistency. Use a BDB Queue?
             throw new NotImplementedException();
         }
