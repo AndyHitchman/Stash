302a995c0915f05a7b7ba55869dc6950d2b386e9
Andy Hitchman <andy@hitchman.net>
2010-01-13 08:00:20 +1100

    Session tracking

--- Stash.Specifications/for_engine/for_persistence_events/given_track/when_enrolling_in_session.cs
+++ Stash.Specifications/for_engine/for_persistence_events/given_track/when_enrolling_in_session.cs
@@ -10,20 +10,6 @@ namespace Stash.Specifications.for_engine.for_persistence_events.given_track
     public class when_enrolling_in_session
     {
         [Test]
-        public void it_should_find_out_if_the_graph_is_already_tracked()
-        {
-            var mockSession = MockRepository.GenerateMock<InternalSession>();
-            var graph = new DummyPersistentObject();
-            var sut = new Track<DummyPersistentObject>(graph, mockSession);
-
-            mockSession.Expect(s => s.GraphIsTracked(graph)).Return(true);
-
-            sut.EnrollInSession();
-
-            mockSession.VerifyAllExpectations();
-        }
-
-        [Test]
         public void it_should_do_nothing_if_the_graph_is_already_tracked()
         {
             var mockSession = MockRepository.GenerateMock<InternalSession>();
@@ -35,6 +21,37 @@ namespace Stash.Specifications.for_engine.for_persistence_events.given_track
             sut.EnrollInSession();
 
             mockSession.AssertWasNotCalled(s => s.Enroll(null), o => o.IgnoreArguments());
+        }
+
+        [Test]
+        public void it_should_enroll_if_the_graph_is_not_tracked()
+        {
+            var mockSession = MockRepository.GenerateMock<InternalSession>();
+            var mockRegistry = MockRepository.GenerateMock<Registry>();
+            var graph = new DummyPersistentObject();
+            var sut = new Track<DummyPersistentObject>(graph, mockSession);
+
+            mockSession.Expect(s => s.GraphIsTracked(graph)).Return(false);
+            mockSession.Expect(s => s.Registry).Return(mockRegistry);
+            mockRegistry.Expect(r => r.GetGraphFor<DummyPersistentObject>()).IgnoreArguments().Return(null);
+
+            sut.EnrollInSession();
+
+            mockSession.AssertWasCalled(s => s.Enroll(sut));
+        }
+
+        [Test]
+        public void it_should_find_out_if_the_graph_is_already_tracked()
+        {
+            var mockSession = MockRepository.GenerateMock<InternalSession>();
+            var graph = new DummyPersistentObject();
+            var sut = new Track<DummyPersistentObject>(graph, mockSession);
+
+            mockSession.Expect(s => s.GraphIsTracked(graph)).Return(true);
+
+            sut.EnrollInSession();
+
+            mockSession.VerifyAllExpectations();
         }
     }
 }
