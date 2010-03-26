f5aa268c49e4c23710db6e6903648cd70cf21539
Andy Hitchman <andy@hitchman.net>
2010-03-26 21:46:44 +1100

    Per HttpRequest session management

--- Stash/Kernel.cs
+++ Stash/Kernel.cs
@@ -17,9 +17,12 @@
 namespace Stash
 {
     using System;
+    using System.Web;
     using BackingStore;
+    using BackingStore.BDB;
     using Configuration;
     using Engine;
+    using Queries;
 
     public static class Kernel
     {
@@ -28,7 +31,9 @@ namespace Stash
         public static IRegistry Registry { get; set; }
 
         public static void Kickstart<TBackingStore>(
-            TBackingStore backingStore, Action<PersistenceContext<TBackingStore>> configurationAction)
+            TBackingStore backingStore,
+            Action<PersistenceContext<TBackingStore>> configurationAction,
+            Action<IRegistry, IBackingStore, IQueryFactory> iocSingletonConfiguration)
             where TBackingStore : IBackingStore
         {
             var registrar = new Registrar<TBackingStore>(backingStore);
@@ -36,12 +41,62 @@ namespace Stash
             registrar.ApplyRegistration();
             Registry = registrar.Registry;
             SessionFactory = new SessionFactory();
+
+            iocSingletonConfiguration(Registry, backingStore, backingStore.QueryFactory);
+        }
+
+        public static void Kickstart(BerkeleyBackingStore backingStore, Action<PersistenceContext<BerkeleyBackingStore>> configurationAction)
+        {
+            Kickstart(backingStore, configurationAction, (registry, store, arg3) => { });
         }
 
         public static void Shutdown()
         {
             Registry.BackingStore.Close();
             Registry = null;
+        }
+
+        public static class HttpRequestManager
+        {
+            /// <summary>
+            /// Stores a <see cref="ISession"/> in <see cref="HttpContext.Items"/>.
+            /// </summary>
+            public static ISession CurrentSession
+            {
+                get { return (ISession)HttpContext.Current.Items["__Stash__CurrentSession"]; }
+                private set { HttpContext.Current.Items["__Stash__CurrentSession"] = value; }
+            }
+
+            /// <summary>
+            /// Use this method to take an action, such as setting-up an IoC container, on each request.
+            /// It is preferable to use the built-in lifecycle support of your container if possible.
+            /// </summary>
+            /// <param name="onBeginRequest"></param>
+            /// <param name="onEndRequest"></param>
+            /// <param name="onUnhandledException"></param>
+            public static void ByActingPerHttpRequest(
+                Action<ISessionFactory> onBeginRequest,
+                Action onEndRequest,
+                Action onUnhandledException)
+            {
+                var application = HttpContext.Current.ApplicationInstance;
+                application.BeginRequest += (sender, args) => onBeginRequest(SessionFactory);
+                application.EndRequest += (o, eventArgs) => onEndRequest();
+                application.Error += (sender, eventArgs) => onUnhandledException();
+            }
+
+            /// <summary>
+            /// This configuration puts a new session into <see cref="HttpContext.Items"/>
+            /// and calls <see cref="ISession.Complete"/> on <see cref="HttpApplication.EndRequest"/>
+            /// and <see cref="ISession.Abandon"/> on <see cref="HttpApplication.Error"/>
+            /// </summary>
+            public static void ByCreatingANewCurrentSession()
+            {
+                ByActingPerHttpRequest(
+                    factory => CurrentSession = factory.GetSession(),
+                    () => CurrentSession.Complete(),
+                    () => CurrentSession.Abandon());
+            }
         }
     }
 }
