a0783c2017cdd1eac3fd6fd13b89cbdfb7fed112
Andy Hitchman <andy@hitchman.net>
2010-03-26 17:40:28 +1100

    Update refs to MVC 2.0

--- Stash.Example/Controllers/HomeController.cs
+++ Stash.Example/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace Stash.Example.Controllers
     [HandleError]
     public class HomeController : Controller
     {
-        public ActionResult About()
-        {
-            return View();
-        }
-
         public ActionResult Index()
         {
             ViewData["Message"] = "Welcome to ASP.NET MVC!";
